f6bdc031bc4be0a59c1de5b20f8200c461b3bda8
Ahmed Lamlih <contact.ahmed.lamlih@gmail.com>
2023-02-17 08:24:14 -0800

    Fix Tasks

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,8 +2,15 @@
 #include <time.h>
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */
+
 int main(void){
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	int last_digit = n % 10;
