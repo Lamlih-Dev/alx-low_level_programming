d945c8046e587aa0c7c24f6b2cd3b41be5dd8ede
Ahmed Lamlih <contact.ahmed.lamlih@gmail.com>
2023-03-01 17:20:45 +0100

    Advanced Tasks

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -23,6 +23,7 @@ void print_line(char *c, int s, int l)
         if (j % 2)
             putchar(' ');
     }
+    
     for (k = 0; k <= s; k++)
     {
         if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
@@ -47,6 +48,7 @@ void print_buffer(char *b, int size)
     for (i = 0; i <= (size - 1) / 10 && size; i++)
     {
         printf("%08x: ", i * 10);
+
         if (i < size / 10)
         {
             print_line(b, 9, i);
@@ -57,6 +59,7 @@ void print_buffer(char *b, int size)
         }
         putchar('\n');
     }
+
     if (size == 0)
         putchar('\n');
 }
